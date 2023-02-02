e2533c2ad10d76829ab9b182a6a3c14dc65b6bc3
Maldon23 <112187215+Maldon23@users.noreply.github.com>
2023-02-02 12:40:55 -0500

    Update level.js

--- level.js
+++ level.js
@@ -143,3 +143,20 @@ function drawGrid(level) {
         ...row.map(type => elt("td", {class: type})))
   ));
 }
+function drawActors(actors) {
+  return elt("div", {}, ...actors.map(actor => {
+    let rect = elt("div", {class: `actor ${actor.type}`});
+    rect.style.width = `${actor.size.x * scale}px`;
+    rect.style.height = `${actor.size.y * scale}px`;
+    rect.style.left = `${actor.pos.x * scale}px`;
+    rect.style.top = `${actor.pos.y * scale}px`;
+    return rect;
+  }));
+}
+DOMDisplay.prototype.syncState = function(state) {
+  if (this.actorLayer) this.actorLayer.remove();
+  this.actorLayer = drawActors(state.actors);
+  this.dom.appendChild(this.actorLayer);
+  this.dom.className = `game ${state.status}`;
+  this.scrollPlayerIntoView(state);
+};
